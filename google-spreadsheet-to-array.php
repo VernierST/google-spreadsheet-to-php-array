08b11eb478be10f216e3e0bc56f4869dba926c4f
David Lim <dlim@vernier.com>
2014-06-19 11:42:17 -0700

    Added new function to handle Google Spreadsheet API version 3 to get cell data. It no longer allows for CSV output and also requires authentication. The new function will parse through the published sheet for the HTML table to recreate a PHP array.

--- google-spreadsheet-to-array.php
+++ google-spreadsheet-to-array.php
@@ -1,8 +1,10 @@
 <?php
 	/* ***********************************************************************
 	*	Written 7 June 2012 by Mason Fabel
-	* Revised 8 June 2012 by David Lim
+	*  Revised 8 June 2012 by David Lim
+	*  Revised 19 June 2014 by David Lim for Google Spreadsheets V3 API
 	*
+	*  V2 Description
 	*	This function takes a url in the form:
 	*	http://spreadsheets.google.com/feeds/cells/$KEY/1/public/values
 	*	where $KEY is the key given to the published version of the
@@ -55,5 +57,54 @@
 			}
 
 		// return the array
+		return $array;
+	}
+
+	/*
+		Get a google spreadsheet and return its contents as an array
+		For version 3.0 of the Google Spreadsheet API, this requires the spreadsheet worksheet
+		to be published as a web page. This function will parse through the generated HTML table
+		to extract spreadsheet contents.
+
+		This is because API v3 requires authentication and we don't want to put credentials in code.
+	*/
+	function google_spreadsheet_to_array_v3($url=NULL) {
+		// make sure we have a URL
+			if (is_null($url)) {
+				return array();
+			}
+
+		// initialize curl
+			$curl = curl_init();
+
+		// set curl options
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_HEADER, 0);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+
+		// get the spreadsheet data using curl
+			$google_sheet = curl_exec($curl);
+
+		// close the curl connection
+			curl_close($curl);
+
+		// parse out just the html table
+			preg_match('/(<table[^>]+>)(.+)(<\/table>)/', $google_sheet, $matches);
+			$data = $matches['0'];
+
+		// Convert the HTML into array (by converting into HTML, then JSON, then PHP array
+			$cells_xml = new SimpleXMLElement($data);
+			$cells_json = json_encode($cells_xml);
+			$cells = json_decode($cells_json, TRUE);
+
+		// Create the array
+			$array = array();
+			foreach ($cells['tbody']['tr'] as $row_number=>$row_data) {
+				$column_name = 'A';
+				foreach ($row_data['td'] as $column_index=>$column) {
+					$array[($row_number+1)][$column_name++] = $column;
+				}
+			}
+
 		return $array;
 	}
